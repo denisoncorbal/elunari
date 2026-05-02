d3dba0f550aa02980b983ad735c5ff49c7c16fc1
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2026-05-02 16:15:51 -0300

    fix(backend): apply uniqueness verification by name on create subject

--- backend/src/main/java/br/nom/corbal/denison/elunari/academic/domain/repository/SubjectRepository.java
+++ backend/src/main/java/br/nom/corbal/denison/elunari/academic/domain/repository/SubjectRepository.java
@@ -1,7 +1,10 @@
 package br.nom.corbal.denison.elunari.academic.domain.repository;
 
 import br.nom.corbal.denison.elunari.academic.domain.model.entity.SubjectEntity;
+import br.nom.corbal.denison.elunari.academic.domain.model.valueobject.SubjectName;
 
 public interface SubjectRepository {
     public SubjectEntity save(SubjectEntity subject);
+
+    public boolean existsByName(SubjectName name);
 }
